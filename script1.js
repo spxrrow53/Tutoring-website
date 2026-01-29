7b0c5c05705b3a592891cb595433ed1f5656b7e6
spxrrow53 <mohammednashadh20055gmail.com>
2026-01-29 20:33:57 +0530

    updated changes

---
+++ script1.js
@@ -0,0 +1,10 @@
+function sendForm(e){
+ e.preventDefault();
+ var formMsg=document.getElementById("formMsg")
+ formMsg.innerText=" Message Sent Successfully✅";
+ formMsg.className = "fontf"
+ 
+
+
+}
+
